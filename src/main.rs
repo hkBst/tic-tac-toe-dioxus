fe315f69fbddb7a6316ec2d3502f6640f1abb82f
Marijn Schouten <mhkbst@gmail.com>
2024-04-09 15:29:44 +0200

    update to dioxus 0.5, update github actions

--- src/main.rs
+++ src/main.rs
@@ -2,49 +2,50 @@ use dioxus::prelude::*;
 use tic_tac_toe::*;
 
 fn main() {
-    dioxus_web::launch(app);
+    launch(app);
 }
 
-fn app(cx: Scope) -> Element {
-    let game = use_ref(cx, Game::new);
+fn app() -> Element {
+    let game = use_context_provider(|| Signal::new(Game::new()));
 
     let board = [Hor::Left, Hor::Mid, Hor::Right].into_iter().flat_map(|h| {
         [Vert::Top, Vert::Mid, Vert::Bottom]
             .into_iter()
             .map(move |v| {
                 rsx!(Field {
-                    game: game.clone(),
+                    game: game,
                     field: FieldName { v, h }
                 })
             })
     });
 
-    cx.render(rsx!(
+    rsx!(
         div { class: "game",
-            div { class: "board", board }
-            div { class: "state", game.with(|g| g.state().to_string()) }
+              div { class: "board", {board} }
+              div { class: "state", {game.with(|g| g.state().to_string())} }
         }
-    ))
+    )
 }
 
-#[inline_props]
+#[component]
 #[allow(non_snake_case)]
-fn Field(cx: Scope, game: UseRef<Game>, field: FieldName) -> Element {
-    let class = match game.with(|g| g.get(*field).0) {
+fn Field(game: Signal<Game>, field: FieldName) -> Element {
+    let class = match game.with(|g| g.get(field).0) {
         Some(Side::X) => "cell side-X",
         Some(Side::O) => "cell side-O",
         None => "cell",
     };
     let action = move |_| {
         game.with_mut(|g| {
-            g.act(*field);
+            g.act(field);
         })
     };
-    let value = game.with(|g| g.get(*field).to_string());
+    let value = game.with(|g| g.get(field).to_string());
 
-    cx.render(rsx!(div {
-        class: class,
-        onclick: action,
-        value
-    }))
+    rsx!(
+        div { class: class,
+              onclick: action,
+              {value}
+        }
+    )
 }
